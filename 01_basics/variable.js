ca8de77aa4dd405abab83edf4b47688d140268b3
mindfreak09 <pranoychy09@gmail.com>
2025-02-14 17:19:04 +0000

    add variable

---
+++ 01_basics/variable.js
@@ -0,0 +1,10 @@
+const accountId= 144553
+let accountEmail="pranoy@gmail.com"
+var accountPassword="12345"
+accountCity="kolkata"
+
+accountEmail="oyepranoy@gmail.com"
+accountPassword="21212121"
+accountCity="shanghai"
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity])
