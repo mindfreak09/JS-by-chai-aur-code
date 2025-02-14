6b8da841b5e38e7e3490ca8c5b82693892c0591b
mindfreak09 <pranoychy09@gmail.com>
2025-02-14 17:19:26 +0000

    add conversion and datatypes

---
+++ 01_basics/conversion.js
@@ -0,0 +1,27 @@
+//let score= "33";
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+//let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+
+// "33"-->> 33
+// "33abc"---> NaN
+// TRUE-- > 1  FALSE    ----> 0
+//
+
+//let isLoggedIn ="pranoy";
+
+//let booleanisLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanisLoggedIn)
+// 1--> true;
+//0--> false;
+//"who"--> true
+//"" --> false
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+
+console.log(stringNumber)
+console.log(typeof stringNumber)
---
+++ 01_basics/datatype.js
@@ -0,0 +1,12 @@
+"use strict"// treat all js code as newer version
+
+//alert(3+3) // we are using nodejs not browser
+
+let name="pranoy"
+let age=14 // numbwe --> 2 to 53
+let isLoggedIn= false
+//null ---> standalone value
+
+
+//object --> 
+console.log(typeof age)
