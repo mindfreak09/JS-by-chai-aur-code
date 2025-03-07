ebae4186ba1a371f67b196bec29dc8313b272f1d
mindfreak09 <pranoychy09@gmail.com>
2025-03-07 07:05:39 +0000

    stack and heap memory

---
+++ 01_basics/memorytype.js
@@ -0,0 +1,16 @@
+let myGithubname= "mindfreak09x"
+let anothername= myGithubname
+anothername="pranoy_09x"
+
+console.log(myGithubname);
+console.log(anothername);
+
+let userOne ={
+    email: "user@gmail.com",
+    upi:"user@ibl"
+}
+let userTwo =userOne
+userTwo.email="pranoy@gmail.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
